734b7ac37b0fb65d3f87a97ddb5942f1d92c894c
RAMAKRISHNA DEVARAKONDA <ramakrishnadevarakonda@RAMAKRISHNAs-iMac.local>
2021-01-07 13:32:14 -0600

    project is ready

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,3 +1,16 @@
+var city = data.map(city=> city.city);
+var state = data.map(state=>state.state);
+var country = data.map(country=>country.country);
+var shape = data.map(shape=>shape.shape);
+var uniqueCity = city.filter((value,index,city)=>city.indexOf(value)===index);
+var uniqueState = state.filter((value,index,state)=>state.indexOf(value)===index);
+var uniqueShape = shape.filter((value,index,shape)=>shape.indexOf(value)===index);
+console.log(city);
+console.log(uniqueCity);
+console.log(uniqueState);
+console.log(country);
+console.log(uniqueShape);
+
 // from data.js
 var tableData = data;
 //console.log(tableData)
@@ -27,9 +40,30 @@ button.on('click',function(){
     tbody.html('');
     var inputElement=d3.select('#datetime');
     var inputValue = inputElement.property('value');
+
+    var dropdownMenu = d3.select("#selDataset");
+    var inputCity = dropdownMenu.property("value");
+    console.log(inputCity);
+    
+    var dropdownMenu1 = d3.select("#selState");
+    var inputState = dropdownMenu1.property("value");
+    console.log(inputState);
+
+    var dropdownMenu2 = d3.select("#selCountry");
+    var inputCountry = dropdownMenu2.property("value");
+    console.log(inputCountry);
+
+    var dropdownMenu3 = d3.select("#selShape");
+    var inputShape = dropdownMenu3.property("value");
+    console.log(inputShape);
+
     //console.log(inputValue);
-    var filteredData = tableData.filter(ufo => ufo.datetime === inputValue);
-    //console.log(filteredData);
+    var filteredData = tableData.filter(
+        ufo => ufo.datetime === inputValue || 
+        ufo.city === inputCity || 
+        ufo.state === inputState ||
+        ufo.country == inputCountry ||
+        ufo.shape === inputShape);
 
     filteredData.forEach((selections)=>{
         //console.log(selections);
